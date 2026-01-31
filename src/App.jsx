220db50df8508180f05645738adab6d8bc3d9941
mattiamonti <mattiamonti2001@gmail.com>
2026-01-31 16:37:54 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -178,7 +178,7 @@ export default function App() {
             toggleComplete(visibleTasks[focusedTaskIndex].id);
           }
           break;
-        case "m":
+        case "e":
           if (visibleTasks[focusedTaskIndex]) {
             handleEditOpen(visibleTasks[focusedTaskIndex]);
           }
